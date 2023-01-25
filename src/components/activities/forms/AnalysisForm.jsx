18c1c523e25381fd2823eb47013da6601a986b3d
Christian Buggle <github@buggle.net>
2023-01-25 15:28:38 +0100

    Enhance AnalysisForm
    * Add Input “CHMO-ID”, Select for “AnalysisType” in AnalysisForm.
    * Add constant analysisTypeOptions.

--- src/components/activities/forms/AnalysisForm.jsx
+++ src/components/activities/forms/AnalysisForm.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-
 import { ListGroupItem, FormGroup, Label, Input } from 'reactstrap'
+import Select from 'react-select'
+
+import { analysisTypeOptions } from '../../../constants/dropdownOptions/analysisTypeOptions'
 
 
-const AnalysisForm = ({action, onWorkupChange}) => {
+const AnalysisForm = ({ action, onWorkupChange }) => {
   return (
     <ListGroupItem>
       <FormGroup>
+        <Label>Type</Label>
+        <Select
+          name="analysis_type"
+          options={analysisTypeOptions}
+          value={analysisTypeOptions.find(option => option.value === action.workup['analysis_type'])}
+          onChange={selectedOption => onWorkupChange({ name: 'analysis_type', value: selectedOption.value })}
+        />
+        <Label>CHMO ID</Label>
+        <Input
+          type="textarea"
+          value={action.workup['chmo_id']}
+          placeholder="CHMO Id"
+          onChange={event => onWorkupChange({ name: 'chmo_id', value: event.target.value })}
+        />
         <Label>Nummer</Label>
         <Input
           type="textarea"
---
+++ src/constants/dropdownOptions/analysisTypeOptions.js
@@ -0,0 +1,18 @@
+// The subset of available ORD AnalysisType options we want to use.
+export const analysisTypeOptions = [{
+  label: 'Thin Layer Chromatography (TLC)',
+  value: 'TLC'
+}, {
+  label: 'Gas Chromatography (GC)',
+  value: 'GC'
+}, {
+  label: 'High Performance Liquid Chronatography (HPLC)',
+  value: 'HPLC'
+}, {
+  label: 'Combined GC/MS (GC/MS)',
+  value: 'GCMS'
+}, {
+  label: 'Combined LC/MS (LCMS)',
+  value: 'LCMS'
+}
+]
