8b1380a5f9a1c96b1abd5c5ecbb5cb291ee0faa7
Joachim Tillessen <tillessen@emjot.de>
2023-02-01 14:37:42 +0100

    make type selection panel for conditions more readable

--- src/components/utilities/TypeSelectionPanel.jsx
+++ src/components/utilities/TypeSelectionPanel.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Button } from "reactstrap";
+import {Button, Row, Col} from "reactstrap";
 import { actionTypeClusters } from "../../constants/actionTypeClusters";
 import { conditionTypeClusters } from '../../constants/conditionTypeClusters';
 
 const TypeSelectionPanel = ({ onSelect, selectionType }) => {
-    const clusters = (selectionType === 'action') ? actionTypeClusters : conditionTypeClusters
-    return (
-        <div className='type-selection-panel row'>
-            {clusters.map((cluster, cIndex) => (
-                <div key={cluster.id + cIndex} className='type-selection-panel__cluster col-4'>
-                    <h6>{cluster.label}</h6>
-                    {cluster.types.map((type, tIndex) => (
-                        <Button
-                            key={type.id + tIndex}
-                            onClick={onSelect(type.action)}
-                            className={'col-12 btn-' + selectionType}
-                        >
-                            {type.createLabel}
-                        </Button>
-                    ))}
-                </div>
-            ))}
-        </div>
-    );
+  const clusters = (selectionType === 'action') ? actionTypeClusters : conditionTypeClusters
+  const colWidth = (selectionType === 'action') ? 4 : 6
+  return (
+    <Row className='type-selection-panel'>
+      {clusters.map((cluster, cIndex) => (
+        <Col key={cluster.id + cIndex} className={'type-selection-panel__cluster col-' +colWidth}>
+          <h6>{cluster.label}</h6>
+          {cluster.types.map((type, tIndex) => (
+            <Button
+              key={type.id + tIndex}
+              onClick={onSelect(type.action)}
+              className={'col-12 btn-' + selectionType}
+            >
+              {type.createLabel}
+            </Button>
+          ))}
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default TypeSelectionPanel;
