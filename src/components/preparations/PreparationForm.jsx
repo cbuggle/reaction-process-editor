073bbb7e58439113bdd16625c28435adae224eb0
Joachim Tillessen <tillessen@emjot.de>
2023-05-15 21:42:32 +0200

    Validate preparations form to make sample mandatory

--- src/components/preparations/PreparationForm.jsx
+++ src/components/preparations/PreparationForm.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useMemo } from 'react'
-import { Button, Label, Input, Form, FormGroup } from 'reactstrap'
+import React, { useState } from 'react'
+import { Label, Input, Form, FormGroup, FormFeedback } from 'reactstrap'
 import Select from 'react-select'
 
-import { useReactionsFetcher } from '../../fetchers/ReactionsFetcher'
 import FormButtons from "../utilities/FormButtons";
 
 const PreparationForm = ({ preparation, preparationOptions, onSave, onCancel }) => {
 
-  const api = useReactionsFetcher()
-
   const [preparationForm, updatePreparationForm] = useState(preparation || {details: ''})
+  const [formIncomplete, setFormIncomplete] = useState(false)
+
 
   const sampleOptions = preparation ? preparationOptions.prepared_samples : preparationOptions.unprepared_samples
 
@@ -18,10 +17,17 @@ const PreparationForm = ({ preparation, preparationOptions, onSave, onCancel }) 
     updatePreparationForm(prevState => ({
       ...prevState, [name]: value
     }));
+    if(formIncomplete && name === 'sample_id') {
+      setFormIncomplete(!value)
+    }
   }
 
   const handleSave = (e) => {
-    onSave(preparationForm)
+    if (!!preparationForm.sample_id) {
+      onSave(preparationForm)
+    } else {
+      setFormIncomplete(true)
+    }
   }
 
   return (
@@ -30,14 +36,17 @@ const PreparationForm = ({ preparation, preparationOptions, onSave, onCancel }) 
         <Label>Sample</Label>
         { }
         <Select
-          className="react-select--overwrite"
-          classNamePrefix="react-select"
+          className={'react-select--overwrite' + (formIncomplete ? ' is-invalid' : '')}
+          classNamePrefix='react-select'
           name="sample_id"
           isDisabled={!!preparation}
           options={sampleOptions}
           value={sampleOptions.filter(option => (option.value === preparationForm.sample_id))}
           onChange={selectedOption => onInputChange({ name: 'sample_id', value: selectedOption.value })}
         />
+        <FormFeedback>
+          Please select a sample!
+        </FormFeedback>
       </FormGroup>
       <FormGroup>
         <Label>Preparations</Label>
