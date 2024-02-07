ee4e5af724da674ba46020b88a1d45decadf397a
Joachim Tillessen <tillessen@emjot.de>
2024-02-07 13:48:07 +0100

    Move VesselModalButton into Form Section Component

--- src/components/steps/StepForm.jsx
+++ src/components/steps/StepForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { Input } from "reactstrap";
 
 import FormButtons from "../utilities/FormButtons";
-import VesselModalButton from "../vessels/VesselModalButton";
+import VesselFormSection from "../vessels/VesselFormSection";
 import { VesselOptions } from "../../contexts/VesselOptions";
 import VesselDecorator from "../../decorators/VesselDecorator";
 import { Label } from "reactstrap";
@@ -11,14 +11,14 @@ const StepForm = ({ processStep, nameSuggestionOptions, onSave, onCancel }) => {
   const vessels = useContext(VesselOptions);
 
   const assignVessel = (vesselId) => {
-    setVessel(vessels.find((vessel) => vessel.id === vesselId));
+    setCurrentVessel(vessels.find((vessel) => vessel.id === vesselId));
   };
 
   const [stepName, setStepName] = useState(processStep?.name);
-  const [vessel, setVessel] = useState(processStep?.vessel);
+  const [currentVessel, setCurrentVessel] = useState(processStep?.vessel);
 
   const handleSave = () => {
-    onSave(stepName, vessel.id);
+    onSave(stepName, currentVessel.id);
   };
 
   const renderNameSuggestionSelect = () => {
@@ -57,33 +57,18 @@ const StepForm = ({ processStep, nameSuggestionOptions, onSave, onCancel }) => {
         />
         {renderNameSuggestionSelect()}
       </div>
-      <div className="form-section form-section--step mb-3">
-        <div className="d-flex justify-content-between align-self-center">
-          <Label
-            className={
-              "col-form-label" +
-              (!!processStep?.vessel ? "" : " label--disabled")
-            }
-          >
-            {VesselDecorator.vesselSingleLine(vessel) || "No vessel assigned"}
-          </Label>
-          <div className="optional-form-group__open-controls">
-            <div className="d-grid gap-2">
-              <VesselModalButton
-                onSelectVessel={assignVessel}
-                typeColor="step"
-                buttonLabel={!!processStep?.vessel ? "Change" : "Set"}
-                scope={"Step" + (stepName ? ' "' + stepName + '"' : "")}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <VesselFormSection
+        currentVessel={currentVessel}
+        onSelectVessel={assignVessel}
+        typeColor="step"
+        buttonLabel={!!currentVessel ? "Change" : "Set"}
+        scope={"Step" + (stepName ? ' "' + stepName + '"' : "")}
+      />
       <FormButtons
         onSave={handleSave}
         onCancel={onCancel}
         type="step"
-        disableSave={!vessel || !stepName}
+        disableSave={!currentVessel || !stepName}
       />
     </>
   );
