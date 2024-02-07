a64ebfab8c9dc7f9ea14279c5563ef11b86ff373
Joachim Tillessen <tillessen@emjot.de>
2024-02-07 21:51:43 +0100

    Fix inconsistent button label

--- src/components/activities/ActivityCreator.jsx
+++ src/components/activities/ActivityCreator.jsx
@@ -1,49 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Button } from "reactstrap";
 
 import ActivityCard from "./ActivityCard";
 import IconButton from "../utilities/IconButton";
 
 import { useReactionsFetcher } from "../../fetchers/ReactionsFetcher";
-import { SubFormController, SubFormToggle } from '../../contexts/SubFormController';
+import {
+  SubFormController,
+  SubFormToggle,
+} from "../../contexts/SubFormController";
 
-const ActivityCreator = ({ processStep, preconditions, insertNewBeforeIndex, onClose }) => {
-
-  const api = useReactionsFetcher()
-  const [displayState, setDisplayState] = useState('buttons')
+const ActivityCreator = ({
+  processStep,
+  preconditions,
+  insertNewBeforeIndex,
+  onClose,
+}) => {
+  const api = useReactionsFetcher();
+  const [displayState, setDisplayState] = useState("buttons");
 
   const save = (actionForm) => {
-    setDisplayState('buttons')
-    api.createActivity(processStep.id, actionForm, insertNewBeforeIndex)
-    if (typeof onClose === 'function') {
-      onClose()
+    setDisplayState("buttons");
+    api.createActivity(processStep.id, actionForm, insertNewBeforeIndex);
+    if (typeof onClose === "function") {
+      onClose();
     }
-  }
+  };
 
-  const cancel = () => setDisplayState('buttons')
+  const cancel = () => setDisplayState("buttons");
 
-  const createActivity = () => setDisplayState('action')
+  const createActivity = () => setDisplayState("action");
 
-  const createCondition = () => setDisplayState('condition')
+  const createCondition = () => setDisplayState("condition");
 
   return (
     <SubFormController.Provider value={SubFormToggle()}>
       {/* The Context Provider must wrap everything. Having it only in the conditional where it belongs yields a */}
       {/* "Rendered more hooks than during the previous render Error" */}
-      {displayState === 'buttons' ?
+      {displayState === "buttons" ? (
         <div className="activity-creator d-flex justify-content-between align-items-center">
           <div className="d-grid gap-2 d-md-flex">
-            <Button color='action' onClick={createActivity}>New Activity</Button>
-            <Button color='condition' onClick={createCondition}>Change Condition</Button>
+            <Button color="action" onClick={createActivity}>
+              New Action
+            </Button>
+            <Button color="condition" onClick={createCondition}>
+              Change Condition
+            </Button>
           </div>
-          {onClose &&
-            <IconButton
-              onClick={onClose}
-              icon='times'
-              positive={true}
-            />
-          }
-        </div> :
+          {onClose && (
+            <IconButton onClick={onClose} icon="times" positive={true} />
+          )}
+        </div>
+      ) : (
         <ActivityCard
           type={displayState}
           onSave={save}
@@ -51,7 +59,7 @@ const ActivityCreator = ({ processStep, preconditions, insertNewBeforeIndex, onC
           preconditions={preconditions}
           processStep={processStep}
         />
-      }
+      )}
     </SubFormController.Provider>
   );
 };
