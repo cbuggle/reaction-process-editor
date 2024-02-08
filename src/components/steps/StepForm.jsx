d6dd901df8c0ffbdcada02bcc4d1739f3e9c0bfe
Joachim Tillessen <tillessen@emjot.de>
2024-02-08 17:59:09 +0100

    introduce universal component for autocomplete input

--- src/components/steps/StepForm.jsx
+++ src/components/steps/StepForm.jsx
@@ -6,6 +6,7 @@ import { VesselOptions } from "../../contexts/VesselOptions";
 import VesselDecorator from "../../decorators/VesselDecorator";
 import SingleLineFormGroup from "../utilities/SingleLineFormGroup";
 import { Typeahead } from "react-bootstrap-typeahead";
+import AutoComplete from "../utilities/AutoComplete";
 
 const StepForm = ({ processStep, nameSuggestionOptions, onSave, onCancel }) => {
   const vessels = useContext(VesselOptions);
@@ -14,42 +15,8 @@ const StepForm = ({ processStep, nameSuggestionOptions, onSave, onCancel }) => {
     setCurrentVessel(VesselDecorator.getVesselById(vesselId, vessels));
   };
 
-  const [tempName, setTempName] = useState("");
   const [stepName, setStepName] = useState(processStep?.name || "");
   const [currentVessel, setCurrentVessel] = useState(processStep?.vessel);
-
-  const handleChange = (name) => {
-    console.log("change", JSON.stringify(name), typeof name);
-    handleNameInput(name);
-  };
-
-  const handleInputChange = (name) => {
-    console.log("inputChange", JSON.stringify(name), typeof name);
-    setTempName(name);
-  };
-
-  const handleBlur = () => {
-    console.log("blur", JSON.stringify(tempName), typeof tempName);
-    if (tempName) {
-      handleNameInput(tempName);
-    } else {
-      handleNameInput(stepName ? stepName : "");
-    }
-  };
-
-  const handleNameInput = (name) => {
-    if (typeof name === "string") {
-      setStepName(name);
-    } else if (name.length > 0) {
-      if (typeof name[0] === "string") {
-        setStepName(name[0]);
-      } else {
-        setStepName(name[0].label);
-      }
-    } else {
-      console.log("handleNameInput", JSON.stringify(name), typeof name);
-    }
-  };
 
   const handleSave = () => {
     onSave(stepName, currentVessel.id);
@@ -58,23 +25,13 @@ const StepForm = ({ processStep, nameSuggestionOptions, onSave, onCancel }) => {
   return (
     <>
       <div className="form-section form-section--step mb-3">
-        <SingleLineFormGroup label="Name">
-          {/*<p>
-            stepName: {JSON.stringify(stepName)} {typeof stepName}
-          </p>
-          <p>
-            tempName: {JSON.stringify(tempName)} {typeof tempName}
-          </p>*/}
-          <Typeahead
-            allowNew
-            id="step-name"
-            onBlur={(e) => handleBlur()}
-            onChange={(selected) => handleChange(selected)}
-            onInputChange={(text) => handleInputChange(text)}
-            options={nameSuggestionOptions.map((option) => option.label)}
-            defaultSelected={[stepName]}
-          />
-        </SingleLineFormGroup>
+        <AutoComplete
+          options={nameSuggestionOptions.map((option) => option.label)}
+          value={stepName}
+          onChange={setStepName}
+          domId="step-name-input"
+          label="Name"
+        />
       </div>
       <VesselFormSection
         currentVessel={currentVessel}
