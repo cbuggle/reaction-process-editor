13410b41b82a92b47219ceb3259037645953a121
Joachim Tillessen <tillessen@emjot.de>
2024-03-25 14:19:17 +0100

    Refactor Notifications (#16)
    * Add custom Notifications using bootstrap (replaces react-toastify).
    * Rewrite useActivityValidator as functional component (was: class component ActivityValidator) to allow usage of hooks required for Notifications context.
    * Adapt calls to useActivityValidator in ActivityForm, ActivityCard.
    * Clean css imports.
    
    ---------
    
    Co-authored-by: Christian Buggle <christian@buggle.net>

--- src/fetchers/requestWrapper.jsx
+++ src/fetchers/requestWrapper.jsx
@@ -1,43 +1,52 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import NotificationContext from "../contexts/NotificationContext";
 
-import { toast } from 'react-toastify';
-
-import { apiBasePath, afterSignOutPath, toastAutoCloseOnError } from "../constants";
+import { apiBasePath, afterSignOutPath } from "../constants";
 
 export { useRequestWrapper };
 
 function useRequestWrapper() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { addNotification } = useContext(NotificationContext);
 
-  return { request }
+  return { request };
 
   function authorizationHeader() {
-    return localStorage.bearer_auth_token ? { 'Authorization': `Bearer ${localStorage.bearer_auth_token}` } : {}
+    return localStorage.bearer_auth_token
+      ? { Authorization: `Bearer ${localStorage.bearer_auth_token}` }
+      : {};
   }
 
   function request(method, path, body) {
-    method === 'GET' || window.dispatchEvent(new Event("indicateSave"))
+    method === "GET" || window.dispatchEvent(new Event("indicateSave"));
 
     const requestOptions = {
       method,
-      headers: authorizationHeader()
+      headers: authorizationHeader(),
     };
-    requestOptions.headers['Accept'] = 'application/json';
-    requestOptions.headers['Content-Type'] = 'application/json';
-    requestOptions.headers['Access-Control-Allow-Origin'] = '*';
+    requestOptions.headers["Accept"] = "application/json";
+    requestOptions.headers["Content-Type"] = "application/json";
+    requestOptions.headers["Access-Control-Allow-Origin"] = "*";
 
     if (body) {
       requestOptions.body = JSON.stringify(body);
     }
-    return fetch(apiBasePath + path, requestOptions).catch(() => { return handleRequestFailure() })
+    return fetch(apiBasePath + path, requestOptions).catch(() => {
+      return handleRequestFailure();
+    });
   }
 
   function handleRequestFailure() {
-    localStorage.removeItem('username');
-    localStorage.removeItem('bearer_auth_token');
-    toast.error("Network connection to Backend failed. Is the ELN server running and reachable?",
-      { autoClose: toastAutoCloseOnError , toastId: 666}) // arbitrary toastId prevents duplicates
-    navigate(afterSignOutPath)
+    localStorage.removeItem("username");
+    localStorage.removeItem("bearer_auth_token");
+    addNotification({
+      title: "Network Error",
+      message:
+        "Network connection to Backend failed. Is the ELN server running and reachable?",
+      type: "error",
+    });
+    navigate(afterSignOutPath);
     return;
   }
 }
