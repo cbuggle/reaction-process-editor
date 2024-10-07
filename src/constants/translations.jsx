0396495b820e6e8c57b2e83f44370924c43052eb
Christian Buggle <christian@buggle.net>
2024-10-07 16:58:39 +0200

    Add translations

--- src/constants/translations.jsx
+++ src/constants/translations.jsx
@@ -2,3 +2,16 @@ export const tooltips = {
 	'purification_amount': 'Please add volume for manual purification',
 	'purification_duration': 'Please add the duration of the step for (half)automated purification.'
 }
+
+
+export const translations = {
+	network: {
+		error: {
+			server_not_reachable: {
+				title: "Network Er:ror",
+				message: "Network connection to Backend failed. Is the ELN server running and reachable?",
+				type: "error"
+			}
+		}
+	}
+}
