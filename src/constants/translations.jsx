7e9930c2b4c48e9cc915b13530edcae0f5afea0d
Christian Buggle <christian@buggle.net>
2024-10-09 13:53:27 +0200

    Fix translation

--- src/constants/translations.jsx
+++ src/constants/translations.jsx
@@ -1,5 +1,5 @@
 export const tooltips = {
-	'selection_unavailable': "The current selected is no longer available in the ELN automation setup. Please be careful when editing. Only the currently available automation setup can be selected.",
+	'selection_unavailable': "The selected item is no longer available in the ELN automation setup. Only the currently available automation setup can be selected when editing.",
 	'purification_amount': 'Please add volume for manual purification',
 	'purification_duration': 'Please add the duration of the step for (half)automated purification.'
 }
