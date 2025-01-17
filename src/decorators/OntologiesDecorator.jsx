c13a63f9dc6a7c2bc74f544c543de8c5580d038a
Christian Buggle <christian@buggle.net>
2025-01-17 11:41:12 +0530

    Rename Ontologies ontology_id
    * Rename Ontologies ontology_id (was: chmo_id).

--- src/decorators/OntologiesDecorator.jsx
+++ src/decorators/OntologiesDecorator.jsx
@@ -13,9 +13,9 @@ export default class OntologiesDecorator {
     })
   }
 
-  static find_by = (chmo_id, options) => options.find(option => option.chmo_id === chmo_id)
-  static findAllByChmoId = (chmo_ids, options) => options.filter(option => chmo_ids?.includes(option.chmo_id))
+  static find_by = (ontology_id, options) => options.find(option => option.ontology_id === ontology_id)
+  static findAllByChmoId = (ontology_ids, options) => options.filter(option => ontology_ids?.includes(option.ontology_id))
 
-  static find_all_by = (chmo_ids, options) => options.filter(option => chmo_ids?.includes(option.chmo_id))
+  static find_all_by = (ontology_ids, options) => options.filter(option => ontology_ids?.includes(option.ontology_id))
 
 }
