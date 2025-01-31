f02a5f4cfb4a84382f5df86c18eeb1f7c7e9d5fb
Christian Buggle <christian@buggle.net>
2025-01-31 16:14:58 +0530

    Fix OntologySelectFormGroup et. al

--- src/components/activities/forms/actions/analysis/SpectroscopyForm.jsx
+++ src/components/activities/forms/actions/analysis/SpectroscopyForm.jsx
@@ -6,6 +6,9 @@ import ButtonGroupToggle from '../../formgroups/ButtonGroupToggle'
 import OntologySelectFormGroup from '../../formgroups/OntologySelectFormGroup'
 import OntologyMultiSelectFormGroup from '../../formgroups/OntologyMultiSelectFormGroup'
 
+import OntologySelectFormGroup from '../../formgroups/OntologySelectFormGroup'
+import OntologyMultiSelectFormGroup from '../../formgroups/OntologyMultiSelectFormGroup'
+
 import OptionsDecorator from '../../../../../decorators/OptionsDecorator'
 import OntologiesDecorator from '../../../../../decorators/OntologiesDecorator'
 
